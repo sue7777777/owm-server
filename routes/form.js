346d2dde9e396d8b289c6e5dfdebde98c212644c
sue7777777 <376844757@qq.com>
2019-04-11 15:11:51 +0800

    update: change router tree

--- routes/form.js
+++
@@ -1,8 +0,0 @@
-var express = require('express')
-var router = express.Router()
-
-var formController = require('../controller/form')
-
-router.get('getFormList', formController.getFormList)
-
-module.exports = router
